28bf65acd960431801e60cdae30e1e392622e7c5
davidoso <jdavidosori96@gmail.com>
2019-02-27 11:56:50 -0600

    Delete unnecessary files. SQL query hasn't been updated yet

--- js/my-general-functions.js
+++ js/my-general-functions.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    var extClass, fileType;
+    var ext, extClass, fileType;
 
     // Set focus on keyword input field on page load
     $('#keyword').focus();
@@ -117,7 +117,7 @@ $(document).ready(function() {
                 {"data":"CLAVE"},
                 {"data":"REVISION",
                     "render": function ( data, type, row ) {
-                        // DELETE THIS WHEN GIBRAN UPDATES SLQ QUERY: "N.A." TURNS TO "-"
+                        // DELETE THIS WHEN GIBRAN UPDATES SLQ QUERY: "N.A." TURNS TO "-" REVISAR QUE QUEDE BIEN WEBDOCS Y . EN ARCHIVOS != PDF
                         if(data == "N.A.")
                             data = "-";
                         if(data.charAt(0) == "0")
@@ -133,36 +133,37 @@ $(document).ready(function() {
                 {"data":"CODIGO_TIPO"},
                 {"data":"EXT",
                     "render": function ( data, type, row ) {
-                        var text = "";
-                        var extSubstring = (data == "pdf") ? "pdf" : data.substring(0, 2);
+                        data = data.toLowerCase();              // Transform to lowercase because the SQL query returns some uppercase file extensions
+                        ext = data;                             // ext is needed in "data":"ARCHIVO" render function
+                        var extSubstring = (data == "pdf") ? "pdf" : data.substring(0, 2);          // Group +1 case conditions if the file is not a PDF
+                        var html = "";
                             switch(extSubstring) {
                                 case "pdf":
-                                    text = '<i class="la la-file-pdf-o la-2x text-danger" title="PDF"></i>';
+                                    html = '<i class="la la-file-pdf-o la-2x text-danger" title="PDF"></i>';
                                     extClass = 'danger';
                                     fileType = 'PDF';
                                     break;
                                 case "xl":      // Includes xl, xlsx, xlsb
-                                    text = '<i class="la la-file-excel-o la-2x text-success" title="Excel"></i>';
+                                    html = '<i class="la la-file-excel-o la-2x text-success" title="Excel"></i>';
                                     extClass = 'success';
                                     fileType = 'Excel';
                                     break;
                                 case "do":      // Includes doc, docx, docm
-                                    text = '<i class="la la-file-word-o la-2x text-info" title="Word"></i>';
+                                    html = '<i class="la la-file-word-o la-2x text-info" title="Word"></i>';
                                     extClass = 'info';
                                     fileType = 'Word';
                                     break;
                                 case "pp":      // Includes ppt, pptx, pps, ppsx
-                                    text = '<i class="la la-file-powerpoint-o la-2x text-warning" title="PowerPoint"></i>';
+                                    html = '<i class="la la-file-powerpoint-o la-2x text-warning" title="PowerPoint"></i>';
                                     extClass = 'warning';
                                     fileType = 'PowerPoint';
                                     break;
                                 default:
-                                    text = '<i class="la la-file-o la-2x text-secondary" title="Otro"></i>';
+                                    html = '<i class="la la-file-o la-2x text-secondary" title="Otro"></i>';
                                     extClass = 'secondary';
                                     fileType = 'archivo';
                             }
-                        data = text;
-                        return data;
+                        return html;
                     },
                     "searchable": false,
                     "orderable": false
@@ -170,14 +171,18 @@ $(document).ready(function() {
                 {"data":"ARCHIVO",
                     "render": function ( data, type, row ) {
                         // console.log("Pathfiles:\n" + data);          // Print all pathfiles
-                        if(fileType == "PDF") {
-                            var text = '<a href="' + data + '" target="_blank" class="btn btn-' + extClass + ' btn-sm"><i class="la la-eye my-la-icon" title="Abrir PDF en otra pestaña"></i></a>';
+                        if(ext == "pdf") {
+                            // In some cases, a dot has to be added manually because the SQL query doesn't concatenate the PDF pathfile, the dot (.) and its "pdf" extension
+                            // Example files (look up these keywords): perforadora, a granel, espreas, proteccion civil, quejas del cliente, espacios confinados
+                            if(!data.includes(".", data.length - ext.length - 1)) {
+                                data = data.substring(0, data.length - ext.length) + "." + ext;
+                            }
+                            var html = '<a href="' + data + '" target="_blank" class="btn btn-' + extClass + ' btn-sm"><i class="la la-eye my-la-icon" title="Abrir PDF en otra pestaña"></i></a>';
                         }
                         else {
-                            var text = '<a href="' + data + '" class="btn btn-' + extClass + ' btn-sm"><i class="la la-download my-la-icon" title="Descargar ' + fileType + '"></i></a>';
+                            var html = '<a href="' + data + '" class="btn btn-' + extClass + ' btn-sm"><i class="la la-download my-la-icon" title="Descargar ' + fileType + '"></i></a>';
                         }
-                        data = text;
-                        return data;
+                        return html;
                     },
                     "searchable": false,
                     "orderable": false
